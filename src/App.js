99b574b67723e388add7373419b741732d39de90
kikuchi-syunsuke <kyunto.site@gmail.com>
2022-10-21 13:44:30 +0900

    total、amountの数を監視するcalculateTotalsというreducerを作成しました

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import "./App.css";
 import CartContainer from "./components/CartContainer";
 import Navbar from "./components/Navbar";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { calculateTotals } from "./features/cart/CartSlice";
 
 function App() {
+    const dispatch = useDispatch();
+    const { cartItems } = useSelector((state) => state.cart);
+    useEffect(() => {
+        dispatch(calculateTotals());
+    }, [cartItems]);
+
     return (
         <main>
             <Navbar />
